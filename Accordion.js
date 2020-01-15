994df98abf19113a6d7eca1504379f2ac3287225
PKB2810 <pranay@kiprosh.com>
2020-01-15 17:40:27 +0530

    removed unnecessary code

--- Accordion.js
+++ Accordion.js
@@ -1,52 +1,47 @@
 import React from 'react';
-import {Touchable} from 'react-native';
 import Animated, {Easing} from 'react-native-reanimated';
 import {Text, View, TouchableOpacity, SafeAreaView} from 'react-native';
-import Item from './Item';
+
 const {
-  sub,
   set,
   cond,
   startClock,
   stopClock,
   clockRunning,
   block,
-  timing,
+  timing, //Updates position node by running timing based animation from a given position to a destination determined by toValue. The animation is expected to last duration milliseconds and use easing function that could be set to one of the nodes exported by the Easing object. The frameTime node will also get updated and represents the progress of animation in milliseconds (how long the animation has lasted so far), similar to the time node that just indicates the last clock time the animation node has been evaluated. Both of these variables are expected to be reset before restarting the animation. Finally finished node will be set to 1 when the position reaches the final value or when frameTime exceeds duration.
   debug,
-  add,
   Value,
-  multiply,
-  Clock,
-  divide,
-  concat,
-  interpolate,
+  Clock, //animated node , the value it returns is the current frame timestamp in milliseconds
 } = Animated;
 function runTiming(clock, value, dest) {
   const state = {
     finished: new Value(0),
-    position: value,
+    position: value, //from position given by value
     time: new Value(0),
-    frameTime: new Value(0),
+    frameTime: new Value(0), //frameTime node will also get updated and represents the progress of animation in milliseconds (how long the animation has lasted so far)
   };
 
   const config = {
-    duration: 1000,
-    toValue: dest,
-    easing: Easing.inOut(Easing.cubic),
+    duration: 1000, //animation duration
+    toValue: dest, //to position given by dest
+    easing: Easing.inOut(Easing.cubic), //easing function
   };
-
+  //block nodes can be used if we want to execute several nodes (commands) in a specific sequence
   return block([
+    //check if clock is running already, if not we set variables and start clock
     cond(clockRunning(clock), 0, [
-      set(state.finished, 0),
+      //cond nodes are equivalent of if ... else
+      set(state.finished, 0), //set nodes are equivalent of =
       set(state.time, 0),
       set(state.position, value),
       set(state.frameTime, 0),
       set(config.toValue, dest),
       startClock(clock),
     ]),
-    timing(clock, state, config),
-    cond(state.finished, debug('stop clock', stopClock(clock))),
-    state.position,
+    timing(clock, state, config), //here we start animation using timing which takes state and config variables
+    cond(state.finished, debug('stop clock', stopClock(clock))), //if animation is finished ,we stop clock
+    state.position, //return position of animated variable which will map to this.heightIncrease
   ]);
 }
 export default class Accordion extends React.PureComponent {
